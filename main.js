54e1c1ae03fc04b984386df58ebbbf32195cfdc5
Tanecodes <tanecodes@gmail.com>
2025-03-30 21:10:44 +1000

    ok i think ive done it correctly still no ball showing in the web browser hmm.

--- main.js
+++ main.js
@@ -21,8 +21,23 @@ const ball = {
 
 // drawing the ball with a function abit complicated for me so im gonna leave comments in the function.
 function drawBall() {
+  ctx.beginPath();
   // ctx.beginPath() creates a path so i can draw new canvas items.This is supposed to stop items from connecting.
   // so basically its the start of a new drawing/item/shape. i think.
-  ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+  // so this method is grabbing the location of the ball and setting its radius.
+  // the 0 and Math.PI * 2 properties have something to do with the left and right side of the ball
+  // 0 is the starting point right side.
+  // Math.PI * 2 is the other side? idk lol PI is 180 degrees and times it by 2 makes it 360 degrees.
+  ctx.fillstyle = ball.color;
+  // this is just geabbing the color from the ball object i created.
+  // the color is set inside the object i created earlier which is blue :)
+  ctx.fill();
+  // this is actually setting/filling the color of the ball. 
+  ctx.closePath();
+  // so now we close the path after setting all the instructions inside
+  // literally a wrapper. 
+  // ctx.beginPath() "CODE GOES IN HERE" ctx.closePath();
 };
+
+drawBall();
